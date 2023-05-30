eb72e684effb33576d6ab94d23426c675eff072a
zhufan12 <61943922+zhufan12@users.noreply.github.com>
2023-05-30 08:55:58 +0000

    create Memento

---
+++ behavioral/memento/Memento.java
@@ -0,0 +1,14 @@
+package behavioral.Memento;
+
+public class Memento {
+    private String state;
+ 
+    public Memento(String state){
+       this.state = state;
+    }
+  
+    public String getState(){
+       return state;
+    }  
+    
+}
